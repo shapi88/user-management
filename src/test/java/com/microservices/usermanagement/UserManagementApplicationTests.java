35796f5d2043a967822cbaece465f51c70b8e5f7
shapi88 <71607420+shapi88@users.noreply.github.com>
2023-07-05 15:52:29 +0100

    RM - UMM-5 - remove kafka listener from tests

--- src/test/java/com/microservices/usermanagement/UserManagementApplicationTests.java
+++ src/test/java/com/microservices/usermanagement/UserManagementApplicationTests.java
@@ -2,9 +2,7 @@ package com.microservices.usermanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.annotation.KafkaListener;
 
-//@KafkaListener
 @SpringBootTest
 class UserManagementApplicationTests {
 
